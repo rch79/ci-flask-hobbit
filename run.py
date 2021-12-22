851d1b6d6848f7fdf9740f840ff26483c6bce935
rch79 <rhorta@gmail.com>
2021-12-22 23:27:57 +0000

    Add base html file and careers page

--- run.py
+++ run.py
@@ -4,9 +4,36 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 
-@app.route("/")
+@app.route("/")   # template to display on root folder
 def index():
+    '''
+    renders index.html
+    '''
     return render_template("index.html")
+
+
+@app.route("/about")
+def about():
+    '''
+    renders about page
+    '''
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    '''
+    renders contact page
+    '''
+    return render_template("contact.html")
+
+
+@app.route("/careers")
+def careers():
+    '''
+    renders careers page
+    '''
+    return render_template("careers.html")
 
 
 if __name__ == "__main__":
